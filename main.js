cb80a24fa6214078b61631d034298798f3453a2d
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-11-29 13:27:52 +0100

    added more initial content

---
+++ main.js
@@ -0,0 +1,26 @@
+
+
+// KNAPP SPARA HOROSKOP onclick 
+// Dölj knapp om horoshop inte är sparat
+
+
+// KNAPP UPPDATERA HOROSKOP 
+
+
+// RADERA HOROSKOP 
+
+
+
+function viewHoroscop(){
+    //TODO
+}
+
+
+function saveHoroscop(){
+    //TODO 
+}
+
+
+function getHoroscop(){
+    // att köra efter vi har fått tillbaka true från php horoscop
+}
