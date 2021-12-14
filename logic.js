2c3f5a3097f9a595de91f1f6caeae1f2e12ed7cf
wictoriadrefelt <wictoria.soderlunddrefeldt@gmail.com>
2021-12-14 11:02:19 +0100

    changes

--- logic.js
+++ logic.js
@@ -1,20 +1,89 @@
 window.addEventListener("load", initSite)
+document.getElementById('saveHoroscopebtn').addEventListener('click', saveHoroscope)
 
 async function initSite() {
+    let horoscope = await getHoroscope();
+    displayHoroscope(horoscope)
 
-    let GET = await makeRequest("./api/addHoroscope.php", {method: "GET"})
-    console.log(GET)
+}
+
+
+async function getHoroscope(){
+    try {
+        let result = await makeRequest("./api/handler.php", {method: "GET"})
+        //let result = await makeRequest("./api/handler.php", {method: "GET"})
+        
+        console.log(result)
+    }catch(err){
+        alert('Make sure date of birth is in correct format')
+    }
+        //let result = await makeRequest("./api/handler.php", {method: "GET"})
+        //let result = await makeRequest("./api/handler.php", {method: "GET"})
+        
+        //console.log(result)
+        /*
+        let body = new FormData()
+        body.set("body", JSON.stringify(horoscope))
     
-    let body = new FormData()
-    body.set("body", JSON.stringify(horoscope))
+        let POST = await makeRequest("./api/handler.php", {method: "POST", body })
+        console.log(POST)
+        console.log(body)
+        */
+    }
 
-    let POST = await makeRequest("./api/addHoroscope.php", {method: "POST", body })
-    console.log(POST)
-    console.log(body)
+
+async function saveHoroscope() {
+    let dateOfBirth = document.getElementById('dateOfBirth').value
+    console.log(dateOfBirth)
+    console.log(dateOfBirth.length)
+    if(dateOfBirth.length < 6 && dateOfBirth.length < 6) {
+        document.getElementById('errorText').innerText = 'please type in date of birth in right format '
+        //text. = 'Please make sure digits are in correct format'
+        return
+    }
+        let horoscope = {
+            dateOfBirth
+        }
+ 
+    let body = new FormData();
+    body.set('horoscope', JSON.stringify(horoscope))
+    let result = await makeRequest("./api/handler.php", {method: 'POST', body});
+    console.log(result)
+
+}
+
+async function makeRequest(url, content) {
+    try {
+    let response = await fetch(url, content)
+    if(response.status < 200 || response.status >= 300){
+        throw new Error(response.statusText)
+
+
+
+    }
+        console.log(response)
+        let result = await response.json()
+        //console.log(result)
+        return result
+
+    } catch(err){
+        console.error(err)
+        throw err;
+    }
+
+}
+
+
+async function displayHoroscope(horoscope){
+
+
+
+
 }
 
 // KNAPP SPARA HOROSKOP onclick 
 // Dölj knapp om horoshop inte är sparat. hidden?
+
 
 
 // KNAPP UPPDATERA HOROSKOP 
@@ -40,13 +109,3 @@ function getHoroscop(){
 
 
 
-async function makeRequest(url, option) {
-    try {
-        let response = await fetch(url, option)
-        let result = response.json()
-        return result
-
-    } catch(err) {
-        console.error(err)
-    }
-}
